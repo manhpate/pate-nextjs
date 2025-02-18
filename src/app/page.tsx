e798a271a4ebb2ff437608ef3b7daa782cbff357
manhpate <manhpate@gmail.com>
2025-02-18 08:43:00 +0700

    Mô tả thay đổi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    <div>
+      <h1>Welcome to My Page PATEEEEE</h1>
+      <Image src="/logo.png" alt="Logo" width={200} height={100} />
+    </div>
   );
 }
